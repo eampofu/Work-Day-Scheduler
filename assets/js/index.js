9d609077cffac81054f543d8fa9ceeaea6b7193e
Eddington <eampofu@gmail.com>
2024-01-09 18:16:10 +0200

    addde event handler for the save button to save to localstorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,7 @@ var workHours = [
 	"5PM",
 ];
 var currentHour = parseInt(dayjs().format("HH"));
-console.log("current hour "+currentHour);
+console.log("current hour " + currentHour);
 // handle displaying the time
 function displayTime() {
 	var rightNow = dayjs().format("DD MMM YYYY [at] hh:mm:ss a");
@@ -27,38 +27,41 @@ function generatePlanner() {
 	for (var i = 0; i < hours.length; i++) {
 		const hour = hours[i];
 		const displayHour = workHours[i];
-	//	console.log(displayHour);
-		var status ='';
-        if(hour==currentHour){
-            status="present";
-        }
-        else if (hour<currentHour){
-            status="past";
-        }
-        else if (hour>currentHour){
-            status="future";
-        }
-        else{
-            status="";
-        }
+
+		var status = "";
+		if (hour == currentHour) {
+			status = "present";
+		} else if (hour < currentHour) {
+			status = "past";
+		} else if (hour > currentHour) {
+			status = "future";
+		} else {
+			status = "";
+		}
 		var row = $("<div>");
 		row.addClass("row");
 		var timeBlock = $("<div>");
 		timeBlock.addClass("col-2 time-block hour");
 		timeBlock.text(displayHour);
 		var todoText = $("<textarea>");
-		todoText.addClass("col-8 "+status);
+		todoText.addClass("col-8 " + status);
 		var btnSave = $("<button>");
-        
-        btnSave.append($('<i>').addClass("fas fa-save"))
-		btnSave.addClass("col-2 saveBtn  data-hour=" + hour );
+
+		btnSave.append($("<i>").addClass("fas fa-save"));
+		btnSave.addClass("col-2 saveBtn");
+		btnSave.attr("data-hour", hour);
 
 		row.append(timeBlock);
 		row.append(todoText);
 		row.append(btnSave);
 		scheduleContainer.append(row);
-
-		
 	}
 }
 generatePlanner();
+//event handler for the button click
+$("button").on("click", (event) => {
+	var key = $(event.target).attr("data-hour");
+	var value = $(event.target).siblings("textarea").val();
+	localStorage.setItem(key, value);
+	console.log(key);
+});
